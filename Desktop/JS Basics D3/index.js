686ca65dc7d78c8f7971b81f37dfcb40de74ff97
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-08 11:18:01 +0200

    day3j.s

---
+++ Desktop/JS Basics D3/index.js
@@ -0,0 +1,26 @@
+// addition
+let addition = 8 + 2 
+
+// subtraction
+let subtraction = 5
+
+// multiplication
+let multiplication = 
+// modulus
+// what is 6 divided by 3 - it divides perfecly, it results in the remainder being either 0 or 1
+// you declare modulus with a percentage
+let modulus = 10%2
+
+// increment
+for (let x = 0; x<10;x++)
+
+// conditional statement
+if (addition > subtraction) {
+    console.log ('Addition is greater than subtraction')
+} else {
+    console.log ('Subtraction is greater than addition')
+}
+
+// switch statement
+let today = new Date ().getDay ();
+console.log (today); 
